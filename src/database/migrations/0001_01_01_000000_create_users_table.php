da1ba5b5c259c2099fd8b5aae3715532d8a9a072
s-hatta <hatta.shogo@gmail.com>
2024-11-18 21:23:33 +0900

    Revert "Feature/task3187"

--- src/database/migrations/0001_01_01_000000_create_users_table.php
+++ src/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,9 +17,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('postal_code', 8)->nullable();
-            $table->string('address')->nullable();
-            $table->string('building')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
