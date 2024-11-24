56bd49bdfd032e2d47faab6a7765be52efab7785
s_hatta <hatta.shogo@gmail.com>
2024-11-24 14:17:47 +0900

    add: プロフィール編集画面へのルーティングを追加する。

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', [ItemController::class, 'index']);
 Route::middleware('auth')->group(function () {
     Route::get('sell', [ItemController::class, 'sell']);
+    Route::get('/mypage/profile', [UserController::class, 'edit']);
 });
