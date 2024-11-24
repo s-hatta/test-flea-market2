03271e4a433b1e30553b6b46b1c21df1fe889659
s_hatta <hatta.shogo@gmail.com>
2024-11-24 12:33:36 +0900

    add: アイテム出品画面のルーティングを追加する

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
 
 Route::get('/', [ItemController::class, 'index']);
+Route::middleware('auth')->group(function () {
+    Route::get('sell', [ItemController::class, 'sell']);
+});
