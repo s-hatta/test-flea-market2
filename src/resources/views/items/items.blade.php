ae0379b1bed2a49d1c4d03ee4525bb343f9f969f
s_hatta <hatta.shogo@gmail.com>
2024-11-24 10:29:00 +0900

    add: 売り切れ(stockが0のとき)にSoldと表示する

--- src/resources/views/items/items.blade.php
+++ src/resources/views/items/items.blade.php
@@ -6,9 +6,14 @@
 @section('content')
 <div class="item-container">
     @foreach($items as $item)
-        <div class="item-card">
-            <a href="{{ url('/item/' . $item->id) }}"> 
-                <img src="{{ Storage::url('public/images/items/' . $item->img_url) }}" alt="商品画像" class="item-image">
+        <div class="item-card @if($item->stock == 0) sold-out @endif">
+            <a href="{{ url('/item/' . $item->id) }}">
+                <div class="item-image-wrapper">
+                    <img src="{{ Storage::url('public/images/items/' . $item->img_url) }}" alt="商品画像" class="item-image">
+                    @if($item->stock == 0)
+                        <div class="sold-text">Sold</div>
+                    @endif
+                </div>
             </a>
             <p class="item-name">{{ $item->name }}</p>
         </div>
