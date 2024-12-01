dd17f407b42796150ac5a72ebe2126791edfcf45
s_hatta <hatta.shogo@gmail.com>
2024-12-01 18:28:27 +0900

    refactor: コメントを追加する

--- src/resources/views/items/item_purchase.blade.php
+++ src/resources/views/items/item_purchase.blade.php
@@ -5,16 +5,20 @@
 
 @section('content')
 <div class="purchase-container">
+    {{--商品の詳細--}}
     <div class="purchase-details">
         <div class="item-info-container">
+            {{--商品画像--}}
             <div class="item-image">
                 <img src="{{ Storage::url('public/images/items/' . $item->img_url) }}" alt="{{ $item->name }}">
             </div>
+            {{--商品価格--}}
             <div class="item-info">
                 <h2>{{ $item->name }}</h2>
                 <p class="price">¥{{ number_format($item->price) }}</p>
             </div>
         </div>
+        {{--支払い方法--}}
         <div class="payment-method">
             <label for="payment">支払い方法</label>
             <select id="payment" name="payment">
@@ -23,6 +27,7 @@
                 <option value="card">カード払い</option>
             </select>
         </div>
+        {{--住所--}}
         <div class="shipping-address">
             <h3>配送先</h3>
             <p>〒{{ $user->postal_code }}</p>
@@ -30,6 +35,7 @@
             <a href="{{ url('/purchase/address/' . $item->id) }}" class="change-address">変更する</a>
         </div>
     </div>
+    {{--サマリー--}}
     <div class="purchase-summary">
         <table>
             <tr>
