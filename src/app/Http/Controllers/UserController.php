714ae03967103af0517cf7f909999fbf2557b3d1
s_hatta <hatta.shogo@gmail.com>
2024-12-01 19:57:59 +0900

    change: プロフィール画面に出品した商品と購入した商品情報を渡すようにする

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Item;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return view('mypage/profile');
+        $user = Auth::user();
+        $exhibitedItems = Item::where('user_id', $user->id)->get();
+        $purchasedItems = User::whereHas('orders', function($query) use ($user) {
+            $query->where('user_id', $user->id);
+        })->get();
+        return view('mypage/profile', compact('user', 'exhibitedItems', 'purchasedItems'));
     }
     
     public function edit()
