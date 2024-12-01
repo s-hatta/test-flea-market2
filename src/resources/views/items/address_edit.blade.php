d63338b637d08ae74e8917004864d273022e28fc
s_hatta <hatta.shogo@gmail.com>
2024-12-01 10:45:07 +0900

    add: 住所変更画面のviewを追加する

---
+++ src/resources/views/items/address_edit.blade.php
@@ -0,0 +1,30 @@
+@extends('/layouts.common')
+@section('css')
+    
+@endsection
+@section('title','住所の変更')
+
+@section('content')
+    <div class="wrapper">
+        <h1>住所の変更</h1>
+        <form class="form">
+            {{--郵便番号--}}
+            <div class="form__item">
+                <div class="form__item-label">郵便番号</div>
+                <input class="form__item-input" type="text" name="postal_code">
+            </div>
+            {{--住所--}}
+            <div class="form__item">
+                <div class="form__item-label">住所</div>
+                <input class="form__item-input" type="text" name="address">
+            </div>
+            {{--建物名--}}
+            <div class="form__item">
+                <div class="form__item-label">建物名</div>
+                <input class="form__item-input" type="text" name="building">
+            </div>
+            {{--登録実行--}}
+            <button class="form__submit" type="submit">更新する</button>
+        </form>
+    </div>
+@endsection
