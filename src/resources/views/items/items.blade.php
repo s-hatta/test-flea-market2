6a15d92d1d8d96408f3080a4290a109e04bf71bc
s_hatta <hatta.shogo@gmail.com>
2024-12-04 19:40:38 +0900

    add: 商品一覧を表示する部分をパーツ化

--- src/resources/views/items/items.blade.php
+++ src/resources/views/items/items.blade.php
@@ -12,21 +12,5 @@
         マイリスト
     </button>
 </div>
-<div class="item-container">
-    @if( isset($items) )
-        @foreach($items as $item)
-            <div class="item-card @if($item->stock == 0) sold-out @endif">
-                <a href="{{ url('/item/' . $item->id) }}">
-                    <div class="item-image-wrapper">
-                        <img src="{{ Storage::url('public/images/items/' . $item->img_url) }}" alt="商品画像" class="item-image">
-                        @if($item->stock == 0)
-                            <div class="sold-text">Sold</div>
-                        @endif
-                    </div>
-                </a>
-                <p class="item-name">{{ $item->name }}</p>
-            </div>
-        @endforeach
-    @endif
-</div>
+@include('parts.items', $items)
 @endsection
