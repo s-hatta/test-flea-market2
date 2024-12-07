31c8cab7caa57ff6fe3bfd0e4cac0640deef29f0
s_hatta <hatta.shogo@gmail.com>
2024-12-07 19:36:16 +0900

    add: usersシーダーに住所情報を追加する

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Models\Address;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -23,10 +24,16 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $address = Address::create([
+                'postal_code' => substr_replace(fake()->postcode, '-', 3, 0),
+                'address' => fake()->prefecture() . fake()->city() . fake()->streetAddress(),
+                'building' => fake()->secondaryAddress(),
+            ]);
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'address_id' => $address->id,
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
