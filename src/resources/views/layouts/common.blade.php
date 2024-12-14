09a86fa1f783a1dcc9e8b412dc4b652d2b258707
s_hatta <hatta.shogo@gmail.com>
2024-12-14 14:52:50 +0900

    change: ヘッダ部分のスタイルをcommon.cssから分離

--- src/resources/views/layouts/common.blade.php
+++ src/resources/views/layouts/common.blade.php
@@ -8,6 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/sanitize.css')}}">
     <link rel="stylesheet" href="{{asset('css/common.css')}}">
+    <link rel="stylesheet" href="{{asset('css/parts/header.css')}}">
     @yield('css')
     <title>@yield('title')</title>
 </head>
