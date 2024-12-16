2f70d8bbae50e14fb6f0207febb2902d1137dd7d
s_hatta <hatta.shogo@gmail.com>
2024-12-16 22:33:16 +0900

    add: 名前と住所変更用のバリデーションを追加する

---
+++ src/app/Http/Requests/AddressRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddressRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required',
+            'postal_code' => 'required|regex:/^[0-9]{3}-[0-9]{4}$/',
+            'address' => 'required',
+        ];
+    }
+    
+    public function messages()
+    {
+        return [
+            'name.required' => 'お名前を入力してください',
+            'postal_code.required' => '郵便番号を入力してください',
+            'postal_code.regex' => '郵便番号はハイフンありの8桁で入力してください',
+            'address.required' => '住所を入力してください',
+        ];        
+    }
+}
