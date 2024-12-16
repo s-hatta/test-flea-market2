ee31219f4b513b2b90c907d2e6224e747bddc43e
s_hatta <hatta.shogo@gmail.com>
2024-12-16 23:43:42 +0900

    add: エラーメッセージを追加する

--- src/resources/views/items/item_exhibit.blade.php
+++ src/resources/views/items/item_exhibit.blade.php
@@ -10,10 +10,12 @@
         @csrf
         <div class="form-group">
             <h3>商品画像</h3>
+            <div class=alert>{{$errors->first('img_url')}}</div>
             <input type="file" name="item_image" class="btn">
         </div>
         <div class="form-group">
             <h3>カテゴリー</h3>
+            <div class=alert>{{$errors->first('categories')}}</div>
             <div id="categories">
                 @foreach($categories as $category)
                     <label>
@@ -23,6 +25,7 @@
                 @endforeach
             </div>
             <h3>商品の状態</h3>
+            <div class=alert>{{$errors->first('condition_id')}}</div>
             <select name="condition_id">
                 <option value="">選択してください</option>
                 @foreach($conditions as $condition)
@@ -33,12 +36,15 @@
         <div class="form-group">
             <h2>商品名と説明</h2>
             <h3>商品名</h3>
+            <div class=alert>{{$errors->first('name')}}</div>
             <input type="text" name="name">
             <h3>ブランド名</h3>
             <input type="text" name="brand_name">
             <h3>商品の説明</h3>
+            <div class=alert>{{$errors->first('detail')}}</div>
             <textarea name="detail"></textarea>
             <h3>販売価格</h3>
+            <div class=alert>{{$errors->first('price')}}</div>
             <input type="text" name="price">
         </div>
         <button type="submit" class="btn submit-btn">出品する</button>
