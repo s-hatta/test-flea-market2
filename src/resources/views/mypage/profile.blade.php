ded35d19703240c5fc03f816bfeff0f9d3672489
s-hatta <hatta.shogo@gmail.com>
2024-12-22 22:06:47 +0900

    refactor: コメント追加

--- src/resources/views/mypage/profile.blade.php
+++ src/resources/views/mypage/profile.blade.php
@@ -6,7 +6,9 @@
 
 @section('content')
 <div class="container">
+    {{--プロフィール--}}
     <div class="profile-header">
+        {{--プロフィール画像--}}
         <div class="profile-image">
             @if($user->img_url)
                 <img src="{{ asset('storage/images/users/'.Auth::user()->img_url) }}" alt="プロフィール画像">
@@ -14,16 +16,21 @@
                 <div class="profile-image__placeholder"></div>
             @endif
         </div>
+        {{--ユーザー名--}}
         <h2 class="profile-name">{{ Auth::user()->name }}</h2>
+        {{--プロフィール編集ボタン--}}
         <a href="{{ url('/mypage/profile') }}" class="edit-profile-button">プロフィールを編集</a>
     </div>
+    {{--表示切替用のタブ--}}
     <div class="profile-tabs">
         <button class="tab-button active" data-tab="exhibitions">出品した商品</button>
         <button class="tab-button" data-tab="purchases">購入した商品</button>
     </div>
+    {{--出品した商品--}}
     <div id="exhibitions" class="tab-content active">
         @include('parts.items', ['items'=>$exhibitedItems])
     </div>
+    {{--購入した商品--}}
     <div id="purchases" class="tab-content">
         @include('parts.items', ['items'=>$purchasedItems])
     </div>
