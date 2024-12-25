a69b6e23eb1f75ca71e0546bfad2d30f6c3de9db
s-hatta <hatta.shogo@gmail.com>
2024-12-25 23:54:56 +0900

    fix: 商品購入画面で住所情報がない場合にエラーメッセージを表示する

--- src/app/Http/Requests/PurchaseRequest.php
+++ src/app/Http/Requests/PurchaseRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use App\Models\Item;
 
-class PurchaseRequest extends FormRequest
+class PurchaseRequest extends AddressRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,32 +22,18 @@ class PurchaseRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
+        $rules = parent::rules();
+        return array_merge($rules, [
             'payment_method' => 'required|in:cvs,card',
-        ];
+        ]);
     }
     
     public function messages(): array
     {
-        return [
+        $messages = parent::messages();
+        return array_merge($messages, [
             'payment_method.required' => '支払い方法を選択してください',
             'payment_method.in' => '支払い方法を選択してください',
-        ];
-    }
-    
-    public function withValidator($validator)
-    {
-        $validator->after(function ($validator) {
-            $user = auth()->user();
-            $item = Item::find($this->input('id'));
-            if ($user && $item) {
-                $address = app(PurchaseController::class)->findAddress($user, $item);
-                $this->merge([
-                    'name' => $user->name,
-                    'postal_code' => $address->postal_code,
-                    'address' => $address->address
-                ]);
-            }
-        });
+        ]);
     }
 }
