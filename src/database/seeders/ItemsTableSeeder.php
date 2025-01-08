7cb5aca14ba705b35904ae27e74d2a946362b67d
s-hatta <hatta.shogo@gmail.com>
2025-01-08 22:01:49 +0900

    refactor: インデントを修正する

--- src/database/seeders/ItemsTableSeeder.php
+++ src/database/seeders/ItemsTableSeeder.php
@@ -17,118 +17,117 @@ class ItemsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        
         $this->copyImages();
         $item = Item::factory()->createOne([
-                'name' => '腕時計',
-                'owner_id' => 1,
-                'brand_name' => 'ノーブランド',
-                'condition_id' => '1',
-                'price' => 15000,
-                'detail' => 'スタイリッシュなデザインのメンズ腕時計',
-                'img_url' => '/item_001.jpg',
+            'name' => '腕時計',
+            'owner_id' => 1,
+            'brand_name' => 'ノーブランド',
+            'condition_id' => '1',
+            'price' => 15000,
+            'detail' => 'スタイリッシュなデザインのメンズ腕時計',
+            'img_url' => '/item_001.jpg',
         ]);
         $item->categories()->attach(Category::where('content', 'ファッション')->first());
         $item->categories()->attach(Category::where('content', 'アクセサリ')->first());
         
         $item = Item::factory()->createOne([
-                'name' => 'HDD',
-                'owner_id' => 1,
-                'brand_name' => 'ノーブランド',
-                'condition_id' => '2',
-                'price' => 5000,
-                'detail' => '高速で信頼性の高いハードディスク',
-                'img_url' => '/item_002.jpg',
+            'name' => 'HDD',
+            'owner_id' => 1,
+            'brand_name' => 'ノーブランド',
+            'condition_id' => '2',
+            'price' => 5000,
+            'detail' => '高速で信頼性の高いハードディスク',
+            'img_url' => '/item_002.jpg',
         ]);
         $item->categories()->attach(Category::where('content', '家電')->first());
         
         $item = Item::factory()->createOne([
-                'name' => '玉ねぎ3束',
-                'owner_id' => 1,
-                'brand_name' => 'ノーブランド',
-                'condition_id' => '3',
-                'price' => 300,
-                'detail' => '新鮮な玉ねぎ3束のセット',
-                'img_url' => '/item_003.jpg',
+            'name' => '玉ねぎ3束',
+            'owner_id' => 1,
+            'brand_name' => 'ノーブランド',
+            'condition_id' => '3',
+            'price' => 300,
+            'detail' => '新鮮な玉ねぎ3束のセット',
+            'img_url' => '/item_003.jpg',
         ]);
         $item->categories()->attach(Category::where('content', 'キッチン')->first());
         
         $item = Item::factory()->createOne([
-                'name' => '革靴',
-                'owner_id' => 1,
-                'brand_name' => 'ノーブランド',
-                'condition_id' => '4',
-                'price' => 4000,
-                'detail' => 'クラシックなデザインの革靴',
-                'img_url' => '/item_004.jpg',
+            'name' => '革靴',
+            'owner_id' => 1,
+            'brand_name' => 'ノーブランド',
+            'condition_id' => '4',
+            'price' => 4000,
+            'detail' => 'クラシックなデザインの革靴',
+            'img_url' => '/item_004.jpg',
         ]);
         $item->categories()->attach(Category::where('content', 'ファッション')->first());
         $item->categories()->attach(Category::where('content', 'メンズ')->first());
         
         $item = Item::factory()->createOne([
-                'name' => 'ノートPC',
-                'owner_id' => 1,
-                'brand_name' => 'ノーブランド',
-                'condition_id' => '1',
-                'price' => 45000,
-                'detail' => '高性能なノートパソコン',
-                'img_url' => '/item_005.jpg',
+            'name' => 'ノートPC',
+            'owner_id' => 1,
+            'brand_name' => 'ノーブランド',
+            'condition_id' => '1',
+            'price' => 45000,
+            'detail' => '高性能なノートパソコン',
+            'img_url' => '/item_005.jpg',
         ]);
         $item->categories()->attach(Category::where('content', '家電')->first());
         
         $item = Item::factory()->createOne([
-                'name' => 'マイク',
-                'owner_id' => 1,
-                'brand_name' => 'ノーブランド',
-                'condition_id' => '2',
-                'price' => 8000,
-                'detail' => '高音質のレコーディング用マイク',
-                'img_url' => '/item_006.jpg',
+            'name' => 'マイク',
+            'owner_id' => 1,
+            'brand_name' => 'ノーブランド',
+            'condition_id' => '2',
+            'price' => 8000,
+            'detail' => '高音質のレコーディング用マイク',
+            'img_url' => '/item_006.jpg',
         ]);
         $item->categories()->attach(Category::where('content', '家電')->first());
         
         $item = Item::factory()->createOne([
-                'name' => 'ショルダーバッグ',
-                'owner_id' => 1,
-                'brand_name' => 'ノーブランド',
-                'condition_id' => '3',
-                'price' => 3500,
-                'detail' => 'おしゃれなショルダーバッグ',
-                'img_url' => '/item_007.jpg',
+            'name' => 'ショルダーバッグ',
+            'owner_id' => 1,
+            'brand_name' => 'ノーブランド',
+            'condition_id' => '3',
+            'price' => 3500,
+            'detail' => 'おしゃれなショルダーバッグ',
+            'img_url' => '/item_007.jpg',
         ]);
         $item->categories()->attach(Category::where('content', 'ファッション')->first());
         $item->categories()->attach(Category::where('content', 'レディース')->first());
         
         $item = Item::factory()->createOne([
-                'name' => 'タンブラー',
-                'owner_id' => 1,
-                'brand_name' => 'ノーブランド',
-                'condition_id' => '4',
-                'price' => 500,
-                'detail' => '使いやすいタンブラー',
-                'img_url' => '/item_008.jpg',
+            'name' => 'タンブラー',
+            'owner_id' => 1,
+            'brand_name' => 'ノーブランド',
+            'condition_id' => '4',
+            'price' => 500,
+            'detail' => '使いやすいタンブラー',
+            'img_url' => '/item_008.jpg',
         ]);
         $item->categories()->attach(Category::where('content', 'キッチン')->first());
         
         $item = Item::factory()->createOne([
-                'name' => 'コーヒーミル',
-                'owner_id' => 1,
-                'brand_name' => 'ノーブランド',
-                'condition_id' => '1',
-                'price' => 4000,
-                'detail' => '手動のコーヒーミル',
-                'img_url' => '/item_009.jpg',
+            'name' => 'コーヒーミル',
+            'owner_id' => 1,
+            'brand_name' => 'ノーブランド',
+            'condition_id' => '1',
+            'price' => 4000,
+            'detail' => '手動のコーヒーミル',
+            'img_url' => '/item_009.jpg',
         ]);
         $item->categories()->attach(Category::where('content', 'キッチン')->first());
         
         $item = Item::factory()->createOne([
-                'name' => 'メイクセット',
-                'owner_id' => 1,
-                'brand_name' => 'ノーブランド',
-                'condition_id' => '2',
-                'price' => 2500,
-                'detail' => '便利なメイクアップセット',
-                'img_url' => '/item_010.jpg',
+            'name' => 'メイクセット',
+            'owner_id' => 1,
+            'brand_name' => 'ノーブランド',
+            'condition_id' => '2',
+            'price' => 2500,
+            'detail' => '便利なメイクアップセット',
+            'img_url' => '/item_010.jpg',
         ]);
         $item->categories()->attach(Category::where('content', 'ファッション')->first());
         $item->categories()->attach(Category::where('content', 'レディース')->first());
